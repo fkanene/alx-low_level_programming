e765d75efffc96a988b89c84714b230f0fbd976c
fkanene <sebbamichy@gmail.com>
2022-09-11 13:54:01 -0700

    c programming conditions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,32 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: alway returns 0
- *
+ * Return: 0
  */
 int main(void)
-{		int i;
-	int j;
-
-		for (i = 0; i <= 99; i++)
-						{
-	for (j = i + 1; j <= 99; j++)
-					{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-					putchar(' ');
-																									putchar(j / 10 + '0');
-																									putchar(j % 10 + '0');
-																									if (!(i == 98 && j == 99))
-																									{
-																									putchar(',');
-																																												putchar(' ');
-																																															}
-																																	}
-										}
-					putchar('\n');
-						return (0);
+{
+	int numLeft;
+	int numLast;
+	for (numLeft = 0; numLeft < 100; numLeft++)
+	{
+		for (numLast = 0; numLast < 100; numLast++)
+		{
+			if (numLeft < numLast)
+			{
+				putchar(numLeft / 10 + 48);
+				putchar(numLeft % 10 + 48);
+				putchar(32);
+				putchar(numLast / 10 + 48);
+				putchar(numLast % 10 + 48);
+				if (numLeft < 98)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
