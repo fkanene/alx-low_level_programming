8c12221797200c2301e3ca6f3c1da70a91c6ad5a
fkanene <sebbamichy@gmail.com>
2022-09-13 10:15:04 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Prints _putchar, followed by a new line
- * Return:zero 
+* main - Entry point
+*
+* Description: Prints _putchar, followed by a new line
+* Return:zero 
 */
 int main(void)
 {
 		char pch[] = "_putchar";
 		int msg;
-	
 		for (msg = 0; msg <= 8; msg++)
 		{
 		putchar(pch[msg]);
